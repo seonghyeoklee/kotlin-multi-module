693e8270d5e9369d70f411fe69a10266a45bef2b
seonghyeoklee <dltjdgur327@gmail.com>
2024-10-18 16:01:08 +0900

    chore: modules and gradle settings

--- settings.gradle.kts
+++ settings.gradle.kts
@@ -1,4 +1,27 @@
-plugins {
-    id("org.gradle.toolchains.foojay-resolver-convention") version "0.8.0"
+pluginManagement {
+    repositories {
+        gradlePluginPortal()
+        mavenCentral()
+        maven {
+            url = uri("https://maven.springframework.org/release")
+        }
+        maven {
+            url = uri("https://maven.restlet.com")
+        }
+    }
 }
+
 rootProject.name = "kotlin-multi-module"
+
+include("module-adapters:adapter-http")
+include("module-adapters:adapter-persistence")
+
+include("module-apps:app-api")
+include("module-apps:app-batch")
+
+include("module-commons")
+
+include("module-core:core-domain")
+include("module-core:core-port")
+include("module-core:core-service")
+include("module-core:core-usecase")
